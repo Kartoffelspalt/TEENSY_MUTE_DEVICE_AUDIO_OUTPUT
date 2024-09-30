e62e9b9c918bd371f4ad33f7321379486334eae3
John Deo <162058432+Kartoffelspalt@users.noreply.github.com>
2024-09-30 22:20:44 +0200

    Update Teensy_Mute_Device_Audio_Output.ino

--- Teensy_Mute_Device_Audio_Output.ino
+++ Teensy_Mute_Device_Audio_Output.ino
@@ -1,48 +1,46 @@
 #include <Bounce2.h>
 #include <Keyboard.h>
 
-const int buttonPin = 2;   // Pin für den Button
-const int ledMutePin = 15;  // Pin für die Lampe bei Stummschaltung
-const int ledOnPin = 16;    // Pin für die Lampe bei aktivem Ton
+const int buttonPin = 2;   
+const int ledMutePin = 15;
+const int ledOnPin = 16;
 
-bool isMuted = false;      // Statusvariable für die Stummschaltung
-Bounce debouncer = Bounce();  // Bounce-Objekt für das Entprellen erstellen
+bool isMuted = false;
+Bounce debouncer = Bounce();
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);  // Pull-up-Widerstand für den Button aktivieren
-  pinMode(ledMutePin, OUTPUT);       // Lampe für Stummschaltung (Pin 3) als Ausgang definieren
-  pinMode(ledOnPin, OUTPUT);         // Lampe für aktiv (Pin 4) als Ausgang definieren
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledMutePin, OUTPUT);
+  pinMode(ledOnPin, OUTPUT);
   
-  // Anfangszustand der LEDs (Ton nicht stumm, also LED an Pin 4 an)
-  digitalWrite(ledMutePin, LOW);     // Stummschaltlampe aus
-  digitalWrite(ledOnPin, HIGH);      // Aktive Lampe an
+  digitalWrite(ledMutePin, LOW);     
+  digitalWrite(ledOnPin, HIGH);     
 
-  debouncer.attach(buttonPin);       // Pin dem Bounce-Objekt zuweisen
-  debouncer.interval(10);            // Entprell-Intervall auf 10ms setzen
+  debouncer.attach(buttonPin);       
+  debouncer.interval(10);         
 
-  Keyboard.begin();                  // Start der Keyboard-Funktion
+  Keyboard.begin();                  
 }
 
 void loop() {
-  debouncer.update();  // Button Status aktualisieren
+  debouncer.update();  
 
-  // Wenn der Button gedrückt wird (Wechsel von HIGH zu LOW):
   if (debouncer.fell()) {
-    // Stummschaltungsstatus umkehren
+    
     isMuted = !isMuted;
 
-    // Mute-Befehl senden (entspricht der Mute-Taste auf der Tastatur)
+   
     Keyboard.press(KEY_MEDIA_MUTE);
-    delay(100);  // Kurzer Delay, damit der Befehl registriert wird
+    delay(100); 
     Keyboard.release(KEY_MEDIA_MUTE);
 
-    // Je nach Stummschaltungsstatus die entsprechende Lampe einschalten
+
     if (isMuted) {
-      digitalWrite(ledMutePin, HIGH);  // Lampe bei Stummschaltung an (Pin 3)
-      digitalWrite(ledOnPin, LOW);     // Lampe bei aktivem Ton aus (Pin 4)
+      digitalWrite(ledMutePin, HIGH); 
+      digitalWrite(ledOnPin, LOW); 
     } else {
-      digitalWrite(ledMutePin, LOW);   // Lampe bei Stummschaltung aus (Pin 3)
-      digitalWrite(ledOnPin, HIGH);    // Lampe bei aktivem Ton an (Pin 4)
+      digitalWrite(ledMutePin, LOW); 
+      digitalWrite(ledOnPin, HIGH);  
     }
   }
 }
